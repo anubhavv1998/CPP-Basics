7056daa86337f97e177b692db3bcf953f2cd9518
Anubhav Singh <66737188+anubhavv1998@users.noreply.github.com>
2021-01-07 22:43:57 +0530

    Add files via upload

---
+++ table.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"Enter any number to find its table "<<endl;
+    cin>>x;
+    for(int i=1;i<=10;i++)
+    cout<<x*i<<endl;
+    return 0;
+}
