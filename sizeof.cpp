963983c4d7ec36a5f92805567156ac27c4b775a4
Anubhav Singh <66737188+anubhavv1998@users.noreply.github.com>
2021-01-07 22:42:51 +0530

    Add files via upload

---
+++ sizeof.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Size of integer is "<<sizeof(int)<<endl;
+    cout<<"Size of float is "<<sizeof(float)<<endl;
+    cout<<"Size of character is "<<sizeof(char)<<endl;
+    cout<<"Size of double is "<<sizeof(double)<<endl;
+    cout<<"Size of long integer is "<<sizeof(long int)<<endl;
+    cout<<"Size of long long integer is "<<sizeof(long long int)<<endl;
+    cout<<"Size of boolean is "<<sizeof(bool)<<endl;
+    return 0;
+}
