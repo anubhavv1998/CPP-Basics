cdbfd092a0f86a965306036cedc8c979c8335491
Anubhav Singh <66737188+anubhavv1998@users.noreply.github.com>
2021-01-07 22:36:59 +0530

    Add files via upload

---
+++ adultOrNot.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"Enter a number"<<endl;
+    cin>>x;
+    if (x>=18)
+    {
+        cout<<"Adult"<<endl;
+    }
+    else
+    {
+        cout<<"Not Adult";
+    }
+    return 0;    
+}
