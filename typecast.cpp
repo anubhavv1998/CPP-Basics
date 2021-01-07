cf55919cecfa6f2ef325bb9569d6483e3277c4d8
Anubhav Singh <66737188+anubhavv1998@users.noreply.github.com>
2021-01-07 22:45:21 +0530

    Add files via upload

---
+++ typecast.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter two numbers to caluculate division"<<endl;
+    cin>>a>>b;
+    double z=(double)a/b;
+    cout<<"Division is "<<z;
+    return 0;
+}
