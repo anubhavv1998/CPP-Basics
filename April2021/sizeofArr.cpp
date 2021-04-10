a614e9943ca507c18106f76e877f819951afb702
Anubhav Singh <4nu6h4v51ngh@gmail.com>
2021-04-10 19:26:33 +0530

    Create sizeofArr.cpp

---
+++ April2021/sizeofArr.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={10,20,30,40,5,1};
+    int n=sizeof(arr);
+    //int n=sizeof(arr)/sizeof(arr[0]);
+    cout<<n;
+    return 0;
+}
