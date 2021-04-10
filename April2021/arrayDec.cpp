656b6cc872558875335c63c16b0fa22068c0f0a6
Anubhav Singh <4nu6h4v51ngh@gmail.com>
2021-04-10 19:22:48 +0530

    Create arrayDec.cpp

---
+++ April2021/arrayDec.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*int n=6;
+    int arr[n]={10,20,30,2,1,4};*/
+    int arr[6]={10,20,30,2,1,4};
+    //int arr[]={10,20,30,2,1,4};
+    //int arr[]{10,20,30,2,1,4};
+    cout<<arr[0]<<" "<<arr[1]<<" "<<arr[2]<<" "<<arr[3]<<" "<<arr[4]<<" "<<arr[5];
+    return 0;
+}
